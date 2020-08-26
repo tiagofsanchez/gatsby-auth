6a9c9f7788f28e71a44ed4d7a9302e0a1c221b87
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-08-26 11:12:33 +0800

    auth with hardcoded values

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import { Link } from "gatsby"
+import { getUser, isLoggedIn } from "../services/auth"
+
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Hello {isLoggedIn() ? getUser().name : "world"}!</h1>
+      {isLoggedIn() ? (
+        <>
+          You are logged in, so check your{" "}
+          <Link to="/app/profile">profile</Link>
+        </>
+      ) : (
+        <>
+          You should <Link to="app/login">log in</Link> to see our restrict
+          content
+        </>
+      )}
+    </Layout>
+  )
 }
